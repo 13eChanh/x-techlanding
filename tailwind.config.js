a975cd14842e627b5f072fa857ace0112ea950b9
CallmeYuki <143131039+13eChanh@users.noreply.github.com>
2024-10-08 23:24:43 +0700

    web demo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        helvetica: ['Helvetica', 'Arial', 'sans-serif'],
+        helveticaMC: ['HelveticaMC', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
